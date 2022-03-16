b019386c0d77a3bd793f36529fd6960f78242efa
Lee SeoJune <sung07288346@gmail.com>
2022-03-16 11:20:46 +0900

    📑 :: Invite entity

---
+++ src/main/java/io/github/leeseojune53/finder/domain/invite/domain/Invite.java
@@ -0,0 +1,46 @@
+package io.github.leeseojune53.finder.domain.invite.domain;
+
+import io.github.leeseojune53.finder.domain.user.domain.User;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "tbl_invite")
+@Entity
+public class Invite {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "send_user_id")
+    private User sendUser;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "invited_user_id")
+    private User invitedUser;
+
+}
