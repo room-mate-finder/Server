478cdebc67d7bb8cedc8f1ba3e1e8a08235f1540
Lee SeoJune <sung07288346@gmail.com>
2022-03-16 14:09:45 +0900

    ⚡️ :: ErrorCode enum

--- src/main/java/io/github/leeseojune53/finder/global/error/exception/ErrorCode.java
+++ src/main/java/io/github/leeseojune53/finder/global/error/exception/ErrorCode.java
@@ -9,7 +9,10 @@ import lombok.Getter;
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum ErrorCode {
 
+    EXPIRED_TOKEN(401, "GLOBAL-401-1", "Expired token."),
+    INVALID_TOKEN(401, "GLOBAL-401-2", "Invalid token."),
     NOT_FOUND(404, "GLOBAL-404-1", "Not Found."),
+    USER_NOT_FOUND(404, "GLOBAL-404-2", "User Not Found."),
     INTERNAL_SERVER_ERROR(500, "GLOBAL-500-1", "Internal Server Error.");
 
     private final int status;
