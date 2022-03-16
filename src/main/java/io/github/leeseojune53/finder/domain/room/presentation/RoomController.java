6c430ad30da08559ca4fc30ab2071fa215b05cba
Lee SeoJune <sung07288346@gmail.com>
2022-03-16 23:29:26 +0900

    ⚡️ :: DELETE /rooms

--- src/main/java/io/github/leeseojune53/finder/domain/room/presentation/RoomController.java
+++ src/main/java/io/github/leeseojune53/finder/domain/room/presentation/RoomController.java
@@ -2,10 +2,12 @@ package io.github.leeseojune53.finder.domain.room.presentation;
 
 import io.github.leeseojune53.finder.domain.room.presentation.dto.request.UpdateRoomInformationRequest;
 import io.github.leeseojune53.finder.domain.room.presentation.dto.response.RoomInformationResponse;
+import io.github.leeseojune53.finder.domain.room.service.LeaveRoomService;
 import io.github.leeseojune53.finder.domain.room.service.QueryRoomInformationService;
 import io.github.leeseojune53.finder.domain.room.service.UpdateRoomInformationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,6 +22,7 @@ public class RoomController {
 
     private final QueryRoomInformationService queryRoomInformationService;
     private final UpdateRoomInformationService updateRoomInformationService;
+    private final LeaveRoomService leaveRoomService;
 
     @GetMapping("/information")
     public RoomInformationResponse queryRoomInformation() {
@@ -30,6 +33,12 @@ public class RoomController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void updateRoomInformation(@RequestBody UpdateRoomInformationRequest request) {
         updateRoomInformationService.execute(request);
+    }
+
+    @DeleteMapping("/")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void leaveRoom() {
+        leaveRoomService.execute();
     }
 
 }
