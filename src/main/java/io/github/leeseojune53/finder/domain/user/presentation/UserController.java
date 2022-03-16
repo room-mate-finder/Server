e65c2d6cdfbe48f95ae61fef1cf5aeff2a06508e
Lee SeoJune <sung07288346@gmail.com>
2022-03-16 15:58:15 +0900

    ⚡️ :: GET /users/information

--- src/main/java/io/github/leeseojune53/finder/domain/user/presentation/UserController.java
+++ src/main/java/io/github/leeseojune53/finder/domain/user/presentation/UserController.java
@@ -1,21 +1,32 @@
 package io.github.leeseojune53.finder.domain.user.presentation;
 
 import io.github.leeseojune53.finder.domain.user.presentation.dto.response.TokenResponse;
+import io.github.leeseojune53.finder.domain.user.presentation.dto.response.UserInformationResponse;
+import io.github.leeseojune53.finder.domain.user.service.QueryUserInformationService;
 import io.github.leeseojune53.finder.domain.user.service.TokenRefreshService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
+@RequestMapping("/users")
 @RestController
 public class UserController {
 
     private final TokenRefreshService tokenRefreshService;
+    private final QueryUserInformationService queryUserInformationService;
 
-    @PutMapping("/users/auth")
+    @PutMapping("/auth")
     public TokenResponse tokenRefresh(@RequestHeader("Refresh-Token") String token) {
         return tokenRefreshService.execute(token);
+    }
+
+    @GetMapping("/information")
+    public UserInformationResponse queryInformation() {
+        return queryUserInformationService.execute();
     }
 
 }
