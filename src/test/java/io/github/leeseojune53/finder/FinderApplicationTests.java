88ff39b1f22155d2321ce326d942ada8dc811c6c
Lee SeoJune <sung07288346@gmail.com>
2022-03-16 10:00:58 +0900

    📑 :: project initialize

---
+++ src/test/java/io/github/leeseojune53/finder/FinderApplicationTests.java
@@ -0,0 +1,12 @@
+package io.github.leeseojune53.finder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class FinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
