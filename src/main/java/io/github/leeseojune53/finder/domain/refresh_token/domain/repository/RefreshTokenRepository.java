88f843fa1cca59cbb616cbd788b4b44fcc18e83d
Lee SeoJune <sung07288346@gmail.com>
2022-03-16 15:31:14 +0900

    ⚡️ :: findByToken

--- src/main/java/io/github/leeseojune53/finder/domain/refresh_token/domain/repository/RefreshTokenRepository.java
+++ src/main/java/io/github/leeseojune53/finder/domain/refresh_token/domain/repository/RefreshTokenRepository.java
@@ -4,6 +4,9 @@ import io.github.leeseojune53.finder.domain.refresh_token.domain.RefreshToken;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+    Optional<RefreshToken> findByToken(String token);
 }
